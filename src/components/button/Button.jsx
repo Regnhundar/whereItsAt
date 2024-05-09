2afaeff50ad9a18e4d0c3206998b81b030305952
Magnus Nilsson <regnhundar@gmail.com>
2024-05-09 17:19:02 +0200

    swiper-funktionalitet på biljetter

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -6,9 +6,12 @@ function Button({
     e.preventDefault();
     console.log(`You clicked: ${text}`);
   },
+  margin = ""
 }) {
+
+  const extraMargin = margin === "big" ? "button button--big-margin" : "button";
   return (
-    <button className="button" onClick={onClick}>
+    <button className={extraMargin} onClick={onClick}>
       {text}
     </button>
   );
